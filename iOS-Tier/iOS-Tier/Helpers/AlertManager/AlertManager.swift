880e62f1e0bea9ff0af070dbccd08f3970cb7fb8
Miguel Bs <miguel@myki.com>
2022-10-27 20:42:32 +0200

    Added alert manager

--- iOS-Tier/iOS-Tier/Helpers/AlertManager/AlertManager.swift
+++ iOS-Tier/iOS-Tier/Helpers/AlertManager/AlertManager.swift
@@ -5,4 +5,29 @@
 //  Created by Miguel Bou Sleiman on 27.10.22.
 //
 
-import Foundation
+import UIKit
+
+protocol AlertManager {
+    func showAlertWith(title: String, message: String, delay: Double, actions: [String: (() -> Void)])
+}
+
+extension AlertManager where Self: UIViewController {
+    func showAlertWith(title: String, message: String, delay: Double = 0.0, actions: [String: (() -> Void)] = [String: (() -> Void)]()) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        for (title, handler) in actions {
+            let action = UIAlertAction(title: title, style: .default) { _ in
+                handler()
+            }
+            alertController.addAction(action)
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+            alertController.dismiss(animated: true)
+        }))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+            self.present(alertController, animated: true)
+        })
+    }
+}
