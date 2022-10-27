efa5326c9bf36224d5843fca17c531b3f531c853
Miguel Bs <miguel@myki.com>
2022-10-27 20:45:44 +0200

    Created the maps vc + its viewmodel

--- iOS-Tier/iOS-Tier/ViewControllers/MapViewController.swift
+++ iOS-Tier/iOS-Tier/ViewControllers/MapViewController.swift
@@ -5,4 +5,167 @@
 //  Created by Miguel Bou Sleiman on 24.10.22.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, AlertManager {
+    lazy var pullupController: PullUpControl = {
+        let pullUpVC = PullUpControl()
+        pullUpVC.dataSource = self
+        pullUpVC.delegate = self
+        pullUpVC.setupCard(from: view)
+        return pullUpVC
+    }()
+    
+    lazy var vehicleInfoView: VehicleInfoView = {
+        let view = VehicleInfoView()
+        return view
+    }()
+    
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.showsUserLocation = true
+        mapView.register(ClusterPinView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
+        mapView.register(PinView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        mapView.delegate = self
+        return mapView
+    }()
+    
+    private var viewModel: MapViewModel
+    
+    init(viewModel: MapViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBinding()
+        setupView()
+        viewModel.fetchVehicles()
+        viewModel.fetchLocation()
+    }
+    
+    func setupBinding() {
+        
+        viewModel.updateMapHandler = {[weak self] annotations in
+            guard let self = self else { return }
+            for annotation in annotations ?? [] {
+                DispatchQueue.main.async {
+                    self.add(annotation: annotation)
+                }
+            }
+        }
+        
+        viewModel.updateLocationHandler = {[weak self] coordinates in
+            guard let coordinates = coordinates else { return }
+            self?.setMapRegion(for: coordinates)
+        }
+        
+        viewModel.showErrorHandler = {[weak self] title, message in
+            self?.showAlertWith(title: title ?? "Error",
+                                message: message ?? "An error occured, please try again later!")
+        }
+        
+        viewModel.locationErrorHandler = {[weak self] title, message in
+            self?.showPermissionError(with: title, message: message, canRetry: true)
+        }
+        
+    }
+    
+    private func setupView() {
+        view.addSubview(mapView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func updateVehicleInfo(for annotation: MapPin) {
+        vehicleInfoView.populate(title: annotation.title ?? "",
+                                 batteryLevel: (annotation.batteryLevel != nil) ? "\(annotation.batteryLevel!)" : "",
+                                 maxSpeed: (annotation.maxSpeed != nil) ? "\(annotation.maxSpeed!)" : "",
+                                 hasHelmetBox: annotation.hasHelmetBox ?? false)
+    }
+}
+
+// MARK: Location Permission Error View
+extension MapViewController {
+    private func showPermissionError(with title: String, message: String, canRetry: Bool = false) {
+        var actions = [String: (() -> Void)]()
+        if canRetry {
+            let retryAction: (() -> Void) = {[weak self] in
+                self?.viewModel.fetchLocation()
+            }
+            actions = ["Retry" : retryAction]
+        }
+        self.showAlertWith(title: title, message: message, actions: actions)
+    }
+}
+
+// MARK: Map Handlers
+extension MapViewController {
+    private func setMapRegion(for coordinates: CLLocationCoordinate2D) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.07, longitudeDelta: 0.07)
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        mapView.setRegion(region, animated: false)
+    }
+    
+    private func add(annotation: MapPin) {
+        mapView.addAnnotation(annotation)
+    }
+}
+
+// MARK: MKMapViewDelegate
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        switch annotation {
+        case is MKClusterAnnotation:
+            return mapView.dequeueReusableAnnotationView(withIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier, for: annotation)
+        default:
+            return nil
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
+        guard let annotation = annotation as? MapPin else { return }
+        pullupController.show()
+        updateVehicleInfo(for: annotation)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard view is ClusterPinView else { return }
+        pullupController.hide()
+    }
+}
+
+
+// MARK: PullUpViewControl Datasource
+extension MapViewController: PullUpControlDataSource {
+    func pullUpViewController() -> UIViewController {
+        vehicleInfoView
+    }
+    
+    func pullUpViewExpandedViewHeight() -> CGFloat {
+        view.frame.size.height * 0.2
+    }
+}
+
+// MARK: PullUpViewControl Delegate
+extension MapViewController: PullUpControlDelegate {
+    func didDismissPullUpControl() {
+        guard let selectedAnnotation = mapView.selectedAnnotations.first else { return }
+        mapView.deselectAnnotation(selectedAnnotation, animated: true)
+    }
+}
--- iOS-Tier/iOS-Tier/ViewModels/MapViewModel.swift
+++ iOS-Tier/iOS-Tier/ViewModels/MapViewModel.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+import CoreLocation
+
+final class MapViewModel {
+    private var dispatcher: Dispatcher
+    private var scooters: [Vehicle]?
+    private var annotations: [MapPin]?
+    private var currentLocation: CLLocationCoordinate2D? {
+        didSet {
+            updateLocationHandler?(currentLocation)
+        }
+    }
+    
+    private lazy var locationManager: LocationManager = {
+        let locationManager = LocationManager()
+        return locationManager
+    }()
+    
+    var updateMapHandler: (([MapPin]?) -> Void)?
+    var updateLocationHandler: ((CLLocationCoordinate2D?) -> Void)?
+    var locationErrorHandler: ((String, String) -> Void)?
+    var showErrorHandler: ((String?, String?) -> Void)?
+    
+    var berlinCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: 52.5200, longitude: 13.4050)
+    }
+    
+    init(dispatcher: Dispatcher = NetworkDispatcher()) {
+        self.dispatcher = dispatcher
+        setupLocationManagerBindings()
+    }
+    
+    func fetchVehicles() {
+        let request = Vehicles.fetchVehicles
+        dispatcher.execute(request: request, resultType: FetchVehiclesResponse.self) {[weak self] result in
+            switch result {
+            case .success(let data):
+                guard let data = data else {
+                    return
+                }
+                
+                self?.scooters = data.data
+                self?.annotations = data.data.enumerated().map { (index, element) in
+                    return MapPin(long: element.attributes.lng, lat: element.attributes.lat, title: element.id, batteryLevel: element.attributes.batteryLevel, maxSpeend: element.attributes.maxSpeed, hasHelmetBox: element.attributes.hasHelmetBox, tag: index)
+                }
+                
+                self?.updateMapHandler?(self?.annotations)
+                
+            case .failure(let error):
+                self?.showErrorHandler?("Error", error.localizedDescription)
+            }
+        }
+    }
+    
+    func fetchLocation() {
+        locationManager.checkLocationPermission {[weak self] error in
+            if error == nil {
+                // update location
+                guard let location = self?.locationManager.currentLocation else { return }
+                self?.currentLocation = location
+            } else {
+                self?.locationErrorHandler?("Error", error?.localizedDescription ?? "An error occred, please try again later!")
+            }
+        }
+    }
+    
+    private func setupLocationManagerBindings() {
+        locationManager.locationHandler = {[weak self] coordinates in
+            guard let coordinates = coordinates else { return }
+            self?.currentLocation = coordinates
+        }
+        
+        locationManager.locationPermissionErrorHandler = {[weak self] error in
+            guard let error = error else { return }
+            self?.locationErrorHandler?("Error", error.localizedDescription)
+        }
+    }
+}
