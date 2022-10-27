b8c6bc51ee3815e2154a61bbc8b0a6d6a26c1bcf
Miguel Bs <miguel@myki.com>
2022-10-27 20:42:14 +0200

    Created custom pin views for clusters & regular pins

--- iOS-Tier/iOS-Tier/Views/ClusterPinView.swift
+++ iOS-Tier/iOS-Tier/Views/ClusterPinView.swift
@@ -5,4 +5,42 @@
 //  Created by Miguel Bou Sleiman on 25.10.22.
 //
 
-import Foundation
+import MapKit
+
+final class ClusterPinView: MKAnnotationView {
+    internal override var annotation: MKAnnotation? {
+        willSet {
+            newValue.flatMap(configure(with:))
+        }
+    }
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        displayPriority = .defaultHigh
+        collisionMode = .circle
+        centerOffset = CGPoint(x: 0.0, y: -10.0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("\(#function) not implemented.")
+    }
+}
+
+//  MARK: Configuration
+private extension ClusterPinView {
+    func configure(with annotation: MKAnnotation) {
+        guard let annotation = annotation as? MKClusterAnnotation else { return }
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40.0, height: 40.0))
+        let count = annotation.memberAnnotations.count
+        image = renderer.image { _ in
+            UIColor.purple.setFill()
+            UIBezierPath(ovalIn: CGRect(x: 0.0, y: 0.0, width: 40.0, height: 40.0)).fill()
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                              NSAttributedString.Key.font: UIFont.appFont.bold(withSize: 20)!]
+            let text = "\(count)"
+            let size = text.size(withAttributes: attributes)
+            let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
+            text.draw(in: rect, withAttributes: attributes)
+        }
+    }
+}
--- iOS-Tier/iOS-Tier/Views/PinView.swift
+++ iOS-Tier/iOS-Tier/Views/PinView.swift
@@ -5,4 +5,17 @@
 //  Created by Miguel Bou Sleiman on 25.10.22.
 //
 
-import Foundation
+import MapKit
+
+internal final class PinView: MKMarkerAnnotationView {
+    internal override var annotation: MKAnnotation? {
+        willSet { newValue.flatMap(configure(with:)) }
+    }
+}
+
+//  MARK: Configuration
+private extension PinView {
+    func configure(with annotation: MKAnnotation) {
+        clusteringIdentifier = String(describing: PinView.self)
+    }
+}
