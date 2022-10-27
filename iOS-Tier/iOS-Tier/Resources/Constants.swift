d1b048dedaa39b952d7b17af1f2812ce188a8d02
Miguel Bs <miguel@myki.com>
2022-10-27 20:40:58 +0200

    added url constants

--- iOS-Tier/iOS-Tier/Resources/Constants.swift
+++ iOS-Tier/iOS-Tier/Resources/Constants.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+enum APIConstants {
+    static let url = "https://api.jsonstorage.net/v1/json/9ec3a017-1c9d-47aa-8c38-ead2bfa9b339/c284fd9a-c94e-4bfa-8f26-3a04ddf15b47"
+    static let apiKey = "9ef7d5b3-21c7-4a78-a92b-91efef42cabb"
+}
